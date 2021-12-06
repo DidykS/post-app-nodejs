8a584f70e08d30dda8ab114351b2a32f1e2951e2
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-12-06 12:05:57 +0200

    start server

---
+++ server.js
@@ -0,0 +1,19 @@
+// import express
+const express = require('express')
+
+// import path
+const path = require('path')
+
+// initialization of server
+const app = express()
+
+// create PORT
+const PORT = 3000
+
+// createPath function
+const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`)
+
+// start server
+app.listen(PORT, (error) => {
+  error ? console.log(error) : console.log(`Listening port ${PORT}`)
+})
