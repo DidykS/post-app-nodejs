3372f1c586263a41fd406d91c1d031a57651e9f0
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-12-06 12:15:26 +0200

    delete helping functions

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 // import express
 const express = require('express')
 
-// import path
-const path = require('path')
-
 // initialization of server
 const app = express()
 
 // create PORT
 const PORT = 3000
-
-// createPath function
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`)
 
 // main route
 app.get('/', (request, response) => {
