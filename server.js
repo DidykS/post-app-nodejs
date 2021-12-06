6c0add3ad6fe050115b5d397190c78060d81eee6
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-12-06 12:13:44 +0200

    add other routes

--- server.js
+++ server.js
@@ -15,7 +15,19 @@ const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`)
 
 // main route
 app.get('/', (request, response) => {
-  response.send('Hello World')
+  response.send('Hello Worlds')
+})
+
+app.get('/posts', (request, response) => {
+  response.send('Posts page')
+})
+
+app.get('/posts/id:', (request, response) => {
+  response.send('Post page')
+})
+
+app.use((request, response) => {
+  response.status(404).send('Error')
 })
 
 // start server
