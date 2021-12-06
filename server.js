8557783d62b2f97068335d9a002646babc31fbf9
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-12-06 12:27:12 +0200

    send html files

--- server.js
+++ server.js
@@ -1,23 +1,43 @@
 // import express
 const express = require('express')
 
+// import path
+const path = require('path')
+
 // initialization of server
 const app = express()
 
 // create PORT
 const PORT = 3000
 
+// createPath function
+const createPath = (page) =>
+  path.resolve(__dirname, 'html-views', `${page}.html`)
+
 // main route
-app.get('/', (request, response) => {})
+app.get('/', (request, response) => {
+  response.sendFile(createPath('index'))
+})
 
 // posts route
-app.get('/posts', (request, response) => {})
+app.get('/posts', (request, response) => {
+  response.sendFile(createPath('posts'))
+})
 
 // post route
-app.get('/posts/id:', (request, response) => {})
+app.get('/posts/id:', (request, response) => {
+  response.sendFile(createPath('post'))
+})
+
+// add post route
+app.get('/add-post', (request, response) => {
+  response.sendFile(createPath('add-post'))
+})
 
 // error midleware
-app.use((request, response) => {})
+app.use((request, response) => {
+  response.status(404).sendFile(createPath('error'))
+})
 
 // start server
 app.listen(PORT, (error) => {
