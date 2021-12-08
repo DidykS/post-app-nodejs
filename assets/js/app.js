19868a58eaea8f6a9371f0a68ad1f33096c96f35
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-12-08 18:32:31 +0200

    add delete post function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,11 @@
 // delete post
 document.addEventListener('click', (e) => {
   const id = e.target?.dataset?.id || null
-  // if (id) {
-  //   fetch(`/posts/${id}`, {
-  //     method: 'DELETE',
-  //   }).then(() => {
-  //     window.location.href = '/posts'
-  //   })
-  // }
-  console.log(id)
+  if (id) {
+    fetch(`/posts/${id}`, {
+      method: 'DELETE',
+    }).then(() => {
+      window.location.href = '/posts'
+    })
+  }
 })
